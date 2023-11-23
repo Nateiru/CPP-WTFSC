1a1f7d9edbd5693737af5582a96bb3518823c56c
ZhuZiyi <zyzhu2001@gmail.com>
2023-11-24 02:17:58 +0800

    add lock free queue
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>

--- src/thread-pool/thread_pool.h
+++ src/thread-pool/thread_pool.h
@@ -59,6 +59,7 @@ inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
 template<class F, class... Args>
 auto ThreadPool::enqueue(F &&f, Args &&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
   using return_type = typename std::result_of<F(Args...)>::type;
+    // using return_type = std::invoke_result_t<F, Args...>;
 
   auto task = std::make_shared<std::packaged_task<return_type()>>(
                 std::bind(std::forward<F>(f), std::forward<Args>(args)...)
